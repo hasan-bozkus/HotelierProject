5ec2d86b4ec0692fd0b201e4f63b10e6dfc45ff6
Hasan BOZKUŞ <103758391+hasan-bozkus@users.noreply.github.com>
2024-02-21 21:27:24 +0300

    Hotelier Projesi
     Bölüm 1 - Giriş ve Katmanların Oluşturulması: Giriş, projenin oluşturulması, dto katmanı ve frontend kısmı, entitylerin oluşturulması, Paketlerin kurulması, referans haritası, migration, abstract interfaces, generic repository, ef class'ları, service ve manager'lar, manager'ların tamamlanması

---
+++ ApiConsume/HotelProject.DataAccessLayer/EntityFramework/EFServiceDal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HotelProject.DataAccessLayer.Abstract;
+using HotelProject.DataAccessLayer.Concrete;
+using HotelProject.DataAccessLayer.Repositories;
+using HotelProject.EntityLayer.Concrete;
+
+namespace HotelProject.DataAccessLayer.EntityFramework
+{
+    public class EFServiceDal : GenericRepository<Service>, IServicesDal
+    {
+        public EFServiceDal(Context context) : base(context)
+        {
+        }
+    }
+}
