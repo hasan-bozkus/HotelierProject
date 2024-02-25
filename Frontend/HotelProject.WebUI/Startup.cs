6a8cd2963691616ab6c0a684b222ba85d562c2e1
Hasan BOZKUŞ <103758391+hasan-bozkus@users.noreply.github.com>
2024-02-25 21:50:26 +0300

    Hotelier Projesi
    Bölüm 6 - Hizmetler:
    Hizmet Dtoları,
    Hizmet Listesi,
    Hizmet Ekleme ve Dto Kontrolü,
    Hizmetlerin Silinmesi,
    Hizmetlerin Güncellenmesi

--- Frontend/HotelProject.WebUI/Startup.cs
+++ Frontend/HotelProject.WebUI/Startup.cs
@@ -23,6 +23,7 @@ namespace HotelProject.WebUI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddHttpClient();
+            services.AddAutoMapper(typeof(Startup));
             services.AddControllersWithViews();
         }
 
