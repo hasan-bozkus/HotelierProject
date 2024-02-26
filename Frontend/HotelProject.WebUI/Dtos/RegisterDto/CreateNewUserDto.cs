3fa1cf667de82cf9bd4356ca416f8c91f6b00444
Hasan BOZKUŞ <103758391+hasan-bozkus@users.noreply.github.com>
2024-02-26 14:01:14 +0300

    Hotelier Projesi
    Bölüm 7 - Identity Kütüphanesi - Register ve Login:
    Identity Kütüphanesi,
    Register Dto,
    Register İşlemi,
    Login İşlemi

---
+++ Frontend/HotelProject.WebUI/Dtos/RegisterDto/CreateNewUserDto.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelProject.WebUI.Dtos.RegisterDto
+{
+    public class CreateNewUserDto
+    {
+        [Required(ErrorMessage = "Ad alanı gereklidir")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Soyad alanı gereklidir")]
+        public string Surname { get; set; }
+
+        [Required(ErrorMessage = "Kullanıcı adı alanı gereklidir")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "E-Posta alanı gereklidir")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Şifre alanı gereklidir")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Şifre alanı gereklidir")]
+        [Compare("Password", ErrorMessage = "Şifreler uyuşmuyor" )]
+        public string ConfirmPassword { get; set; }
+    }
+}
